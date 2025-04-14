173d2fdad3bbb3b28a08b0bc52924dee5f9deb90
Jean-Paul <jeanpaulaghnatyos@gmail.com>
2025-04-14 20:55:26 +0300

    initial commit

--- mock/auth.ts
+++ mock/auth.ts
@@ -1,19 +1,25 @@
 import { MockMethod } from 'vite-plugin-mock';
+
 import { generateResponse, generateToken } from './mock.util';
 
 const mock: MockMethod[] = [
   {
     url: '/api/login',
     method: 'post',
-    timeout: 0,
+    timeout: 2000,
     response: ({ body }: { body: { email: string; password: string } }) => {
-      const { email, password } = body;
-      if (email === 'academy@gmail.com' && password === 'academy123') {
-        const expiresIn = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365;
-        const accessToken = generateToken({ email, password, expiresIn });
-        return generateResponse({ expiresIn, accessToken });
+      if (body) {
+        const { email, password } = body;
+        if (email === 'academy@gmail.com' && password === 'academy123') {
+          const expiresIn = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365; // 1 year
+          const accessToken = generateToken({ email, password, expiresIn });
+
+          return generateResponse({ expiresIn, accessToken });
+        }
+
+        return generateResponse({}, 401, 'Invalid Credentials!');
       }
-      return generateResponse({}, 401, 'Invalid Credentials!');
+      return generateResponse({}, 500, 'Server Error!');
     },
   },
 ];
